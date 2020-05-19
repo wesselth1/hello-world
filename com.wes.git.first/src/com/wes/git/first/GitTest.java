0e4340895dce54c808d023593a13803e0fb2bc9c
Wessel Thomas <wesse@IDEA-PC>
2020-05-18 20:18:22 -0400

    This represents the complete project dragged into the staged changes area.

---
+++ com.wes.git.first/src/com/wes/git/first/GitTest.java
@@ -0,0 +1,10 @@
+package com.wes.git.first;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is fun");
+	}
+
+}
